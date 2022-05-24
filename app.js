28403b85acd195c3bef7e28d1bd97fdd18fb8738
tokarevkk <tokarevkk@gmail.com>
2022-05-24 14:18:01 +0300

    bug

--- app.js
+++ app.js
@@ -11,11 +11,81 @@ function showTime() {
       min = today.getMinutes(),
       sec = today.getSeconds();
    
-   // // Set AM or PM
-   // const amPm = hour >= 12 ? 'PM' : 'AM'
+   // Set AM or PM
+   const amPm = hour >= 12 ? 'PM' : 'AM';
 
-   time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec}`;
-   setTimeOut(showTime, 1000);
+   time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+   setTimeout(showTime, 1000);
+}
+
+
+
+function addZero(n) {
+   return (parseInt(n, 10) < 10 ? '0' : " ") + n;
 
 }
+
+function setBg() {
+   let today = new Date();
+   hour = today.getHours();
+   if (hour < 10) {
+      // Morning
+      document.body.style.backgroundImage = "url(../img/hills.jpg)";
+      greeting.textContent = 'Good morning';
+      document.body.sttyle.color = 'white';
+   } else if (hour < 18){
+      // Afternoon
+      document.body.style.backgroundImage = "url(../img/hills.jpg)";
+      greeting.textContent = 'Good Afnernoon';
+      document.body.style.color = 'white';
+   } else {
+      //Evening
+      document.body.style.backgroundImage = "url(../img/hills.jpg)";
+      greeting.textContent = 'Good Evening';
+      document.body.sttyle.color = 'white';
+   }
+}
+
+// Name
+function getName() {
+   const name = document.getElementById('name');
+   if (localStorage.getItem('name') === null) {
+      name.textContent = ' [Enter name]';
+   } else {
+      name.textContent = localStorage.getItem('name')
+   }
+   console.log(name);
+}
+
+//SetName
+function setName() {
+   const name = document.getElementById('name');
+   if (e.type === 'keypress') {
+      if (e.whitch == 13 || e.keyCode == 13) {
+         localStorage.setItem('name, e.target.innerText');
+         name.blur();
+      }
+   } else {
+      localStorage.setItem('name, e.target.innerText');
+   }
+}
+
+//Focus
+function getFocus() {
+   if (localStorage.getItem('focus') === null) {
+      focus.textContent = ' [Enter Focus]';
+   } else {
+      focus.textContent = localStorage.getItem('focus')
+   }
+}
+
+
+const name = document.getElementById('name');
+name.addEventListenner('keypress', getName);
+name.addEventListenner('blur', getName);
+
+
 showTime();
+setBg();
+getName();
+getFocus();
